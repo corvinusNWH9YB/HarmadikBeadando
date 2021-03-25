27bbeba5bea869b47224f915c3823eabe1df9e3f
corvinusNWH9YB <mate.fenyvesvolgyi@stud.uni-corvinus.hu>
2021-03-25 21:09:56 +0100

    Init project

---
+++ src/main/java/hu/nwh9yb/harmadik/HarmadikApplication.java
@@ -0,0 +1,13 @@
+package hu.nwh9yb.harmadik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmadikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarmadikApplication.class, args);
+    }
+
+}
---
+++ src/main/java/hu/nwh9yb/harmadik/controller/MainController.java
@@ -0,0 +1,8 @@
+package hu.nwh9yb.harmadik.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+}
