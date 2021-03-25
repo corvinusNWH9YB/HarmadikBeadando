42ef5957cebbfef0ee22cf8c23d18e4f91d75d9f
corvinusNWH9YB <mate.fenyvesvolgyi@stud.uni-corvinus.hu>
2021-03-25 21:23:33 +0100

    a) get oldal

--- src/main/java/hu/nwh9yb/harmadik/controller/MainController.java
+++ src/main/java/hu/nwh9yb/harmadik/controller/MainController.java
@@ -1,8 +1,21 @@
 package hu.nwh9yb.harmadik.controller;
 
+import hu.nwh9yb.harmadik.dto.SimpleResponse;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.MessageFormat;
 
 @RestController
 public class MainController {
+    @GetMapping("/harmadik")
+    public SimpleResponse udvozles(@RequestParam(required = false) String name){
+        if(!StringUtils.hasText(name)){
+            name = "Felhasznalo";
+        }
 
+        return new SimpleResponse(MessageFormat.format("Hello {0}!", name));
+    }
 }
---
+++ src/main/java/hu/nwh9yb/harmadik/dto/SimpleResponse.java
@@ -0,0 +1,11 @@
+package hu.nwh9yb.harmadik.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@AllArgsConstructor
+public class SimpleResponse {
+    String content;
+}
