8b93e5ed936fb38c2b6163947e49600f158c1b6c
corvinusNWH9YB <mate.fenyvesvolgyi@stud.uni-corvinus.hu>
2021-03-25 23:39:07 +0100

    b) POST oldal

--- src/main/java/hu/nwh9yb/harmadik/controller/MainController.java
+++ src/main/java/hu/nwh9yb/harmadik/controller/MainController.java
@@ -2,20 +2,28 @@ package hu.nwh9yb.harmadik.controller;
 
 import hu.nwh9yb.harmadik.dto.SimpleResponse;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.MessageFormat;
 
 @RestController
+@RequestMapping("/harmadik")
 public class MainController {
-    @GetMapping("/harmadik")
+    @GetMapping("")
     public SimpleResponse udvozles(@RequestParam(required = false) String name){
         if(!StringUtils.hasText(name)){
             name = "Felhasznalo";
         }
 
         return new SimpleResponse(MessageFormat.format("Hello {0}!", name));
+    }
+
+    @PostMapping("/megfordit")
+    public SimpleResponse megfordit(@RequestBody(required = false) String value){
+        if(!StringUtils.hasText(value)){
+            value = "Alma a fa alatt";
+        }
+        String reverseValue = new StringBuilder(value).reverse().toString();
+        return new SimpleResponse(reverseValue);
     }
 }
